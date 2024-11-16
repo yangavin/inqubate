8c91cfbd454fece1a7f5f32c0be3f1d0f07bd089
KosiSochi Amobi-Oleka <kosi.amobioleka@gmail.com>
2024-11-15 20:36:30 -0500

    Updating sponsors

--- src/app/Sponsors.jsx
+++ src/app/Sponsors.jsx
@@ -5,6 +5,8 @@ export default function Sponsors() {
       <img src="wealthsimple.png" alt="wealthsimple" className="w-96" />
       <img src="aws.png" alt="aws" className="w-96" />
       <img src="rbc.png" alt="rbc" className="w-96" />{" "}
+      <img src="ripple.png" alt="ripple" className="w-96" />
+      <img src="autodesk.png" alt="autodesk.png" className="w-96" />
       {/* This is rounded to make it look better but logo is off-centered! */}
     </>
   );
