7e30373c70ff075f067da4590fb47a6d3b4b3fa0
Gavin Yan <gavinyan10@gmail.com>
2024-11-18 10:59:58 -0500

    fix navbar styling

--- src/app/Navigation.jsx
+++ src/app/Navigation.jsx
@@ -2,7 +2,7 @@ export default function Navigation({ children, href }) {
   return (
     <a
       href={href}
-      className="hover:border-2 hover:rounded-md px-4 hover:font-bold items-center  justify-center flex w-32 transition-[font-weight] transition-transform hover:-translate-y-1"
+      className="hover:border-2 hover:rounded-md px-4 hover:font-bold items-center  justify-center flex w-32 transition-transform hover:-translate-y-1 py-2"
     >
       <span>{children}</span>
     </a>
